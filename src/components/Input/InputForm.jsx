e6b5f281d31e798c7234fc11d4f43b73100ddf84
WellersonDouglas <wellersondouglasvr@gmail.com>
2022-10-06 22:57:01 +0100

    fazendo conexão com banco de dados

--- src/components/Input/InputForm.jsx
+++ src/components/Input/InputForm.jsx
@@ -1,4 +1,4 @@
-export function InputForm({ name, label, ...props }) {
+export function InputForm({ name, label, error, ...props }) {
   return (
     <div className="flex flex-col">
       <label htmlFor={name} className="text-sm font-bold text-gray-500 mb-2">
@@ -8,8 +8,11 @@ export function InputForm({ name, label, ...props }) {
         {...props}
         name={name}
         id={name}
-        className="p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1 focus:outline-red-500"
+        className={`p-3 border border-gray-700 rounded-xl focus:outline focus:outline-1 focus:outline-red-500 ${
+          error && 'border-red-600'
+        }`}
       />
+      <span className="p-2 text-sm text-red-300">{error}</span>
     </div>
   )
 }
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,8 +1,41 @@
+import axios from 'axios'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 import { ArrowLeft } from 'phosphor-react'
 import { InputForm } from '~/components/Input/InputForm'
 import LogoBranca from '../../assets/logo-fundo-branco.svg'
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Preencha seu nome'),
+  username: Yup.string().required('Preencha seu nome de usuário'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Informe seu e-mail'),
+  password: Yup.string().required('Digite uma senha'),
+})
+
 export function Cadastro() {
+  const formik = useFormik({
+    onSubmit: async (values) => {
+      await axios({
+        method: 'post',
+        baseURL: 'http://localhost:3333',
+        url: '/users',
+        data: values,
+      })
+    },
+    initialValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+    validationSchema,
+  })
+
+  //
+
   return (
     <div>
       <header className="p-4 border-b border-red-300">
@@ -19,12 +52,16 @@ export function Cadastro() {
           <h2 className="text-xl font-bold">Crie sua conta</h2>
         </div>
 
-        <form className="space-y-6 p-4">
+        <form onSubmit={formik.handleSubmit} className="space-y-6 p-4">
           <InputForm
             type="text"
             name="name"
             label="Seu nome"
             placeholder="Digite seu nome"
+            error={formik.errors.name && formik.touched.name}
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
 
           <InputForm
@@ -32,27 +69,40 @@ export function Cadastro() {
             name="username"
             label="Seu nome de usuário"
             placeholder="Digite um nome de usuário"
+            error={formik.errors.username && formik.touched.username}
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
           <InputForm
             type="email"
             name="email"
             label="Seu e-mail"
             placeholder="Digite seu e-mail"
+            error={formik.errors.email && formik.touched.email}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
           <InputForm
             type="password"
             name="password"
             label="Sua senha"
-            placeholder="Digite sua senha"
+            placeholder="Digite uma senha"
+            error={formik.errors.password && formik.touched.password}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
 
-          <a
-            href="/dashboard"
-            className="block w-full text-center text-white bg-red-700 px-6 py-3 rounded-xl transition-colors hover:bg-red-500
+          <button
+            type="submit"
+            disabled={!formik.isValid}
+            className="block w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl transition-colors hover:bg-red-700 disabled:opacity-50 disabled:cursor-not-allowed
           "
           >
             Criar minha conta
-          </a>
+          </button>
         </form>
       </main>
     </div>
